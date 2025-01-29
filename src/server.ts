5a47d3b1db729e0ff465e2488f97fef51f399db6
Kingsley <donchibs59@gmail.com>
2025-01-29 12:19:44 +0100

    Clerk-User

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,9 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import * as dynamoose from "dynamoose";
-import courseRoutes from "./routes/courseRoutes"
+import courseRoutes from "./routes/courseRoutes";
+import { createClerkClient } from "@clerk/express";
+import userClerkRoute from "./routes/userClerkRoute"
 
 dotenv.config();
 
@@ -14,6 +16,10 @@ const isProduction = process.env.NODE_ENV === "Production";
 if (!isProduction) {
   dynamoose.aws.ddb.local();
 }
+
+export const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 const app = express();
 app.use(express.json());
@@ -28,7 +34,8 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.use("/courses", courseRoutes)
+app.use("/courses", courseRoutes);
+app.use("/user/clerk", userClerkRoute);
 
 const port = process.env.PORT || 3000;
 if (!isProduction) {
